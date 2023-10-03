8870326c858cc9cb0cd04bbf0b8b301c8cf04b75
Harendra <112871832+827202@users.noreply.github.com>
2023-10-03 21:50:24 +0530

    Add files via upload

---
+++ 17 Bit Manipulation/BitManipulation.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class BitManipulation{
+    public static void main(String[] args) {
+        System.out.println(5 & 6); //Binary AND
+        System.out.println(5 | 6); //Binary OR
+        System.out.println(5 ^ 6); //Binary XOR
+        System.out.println(~ 6);//Binary NOT
+        System.out.println(5 << 2); //Binary Left Shift=a * 2^b
+        System.out.println( 5 >> 2);//Binary Left Shift=a / 2^b
+    }
+}
+
